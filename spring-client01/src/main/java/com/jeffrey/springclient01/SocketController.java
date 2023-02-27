13b3e692da34b7aa1da3bf4ccab23585e676c1ef
jeffrey <1056015243@qq.com>
2023-02-27 14:27:58 +0800

    jdk-websocket-server-client-3

--- spring-client01/src/main/java/com/jeffrey/springclient01/SocketController.java
+++ spring-client01/src/main/java/com/jeffrey/springclient01/SocketController.java
@@ -3,6 +3,7 @@ package com.jeffrey.springclient01;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
